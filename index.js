a05ec678c1225b7710f58e0807bb3f499ef4ca72
Mayur3145 <88161602+Mayur3145@users.noreply.github.com>
2022-04-20 11:12:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('https://github.com/Mayur3145/PWA_Application/serviceworker.js')
+  navigator.serviceWorker.register('https://mayur3145.github.io/PWA_Application/serviceworker.js')
   .then((registration) => {
   console.log("service worker registered")
   })
